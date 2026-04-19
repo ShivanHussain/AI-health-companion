74e35d2a5bce3d51c8f762930ab63f1b9ee6ad9c
Shivan Hussain <shivanhussain6@gmail.com>
2026-04-19 17:00:25 +0530

    Configure CORS for specific origin and methods

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ connectDB();
 const app = express();
 
 
-app.use(cors());
+
+app.use(cors({
+  origin: "https://ai-health-com.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
